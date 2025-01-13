99c25584b5ad4eee92f024bc6acbc655104903d5
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-13 11:29:06 +0200

    jekabs2

--- controllers/story.php
+++ controllers/story.php
@@ -1,3 +1,3 @@
 <?php
-include('router.php');
+echo "cau";
 ?>
--- index.php
+++ index.php
@@ -1,33 +1,10 @@
 <?php
+
 require "functions.php";
 require "Database.php";
-include('controllers/router.php');
+
+
 
 $config = require("config.php");
-
-
-
 $db = new Database($config["database"]);
-
-$sql = "SELECT * FROM posts";
-$params = [];
-//meklesana
-if(isset($_GET["search_query"]) && $_GET["search_query"] != ""){
-    $search_query = "%" . $_GET["search_query"] . "%";
-    $sql .= " WHERE content LIKE :search_query";
-    $params = ["search_query" => $search_query];
-}
-
-$posts = $db->query($sql, $params)->fetchAll();
-
-$d = "Liels";
-$d .= "suns";
-
-$pageTitle = "Blogs";
-$style = "css/style1.css";
-require "views/index.view.php";
-
-
-
-
-
+require "router.php";
